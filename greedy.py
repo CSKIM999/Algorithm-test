1b8811370cbc33168bfc7bb2c7597a0ae091f165
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2021-05-20 21:45:07 +0900

    21/05/20 21:44
    example  start

--- greedy.py
+++ greedy.py
@@ -220,19 +220,19 @@ from functools import partial
 
 ###################### 최단거리 길찾기 ######################
 
-n,m = 4,7
+# n,m = 4,7
 
-graph = [
-    [1,0,1,1,1,1,1],
-    [1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1],
-    [1,1,1,0,1,0,1]
-]
-x,y=0,0
+# graph = [
+#     [1,0,1,1,1,1,1],
+#     [1,0,1,0,1,0,1],
+#     [1,0,1,0,1,0,1],
+#     [1,1,1,0,1,0,1]
+# ]
+# x,y=0,0
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-count = 0
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+# count = 0
 # def bfs(x,y):
 #     global count
 
@@ -270,29 +270,45 @@ count = 0
 
 # bfs(0,0)
 
-def bfs(x, y):
-    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    queue = deque()
-    queue.append((x, y))
-    # 큐가 빌 때까지 반복하기
-    while queue:
-        x, y = queue.popleft()
-        print(x,y,y)
-        # 현재 위치에서 4가지 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    # 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n - 1][m - 1]
+# def bfs(x, y):
+#     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+#     queue = deque()
+#     queue.append((x, y))
+#     # 큐가 빌 때까지 반복하기
+#     while queue:
+#         x, y = queue.popleft()
+#         print(x,y,y)
+#         # 현재 위치에서 4가지 방향으로의 위치 확인
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             # 미로 찾기 공간을 벗어난 경우 무시
+#             if nx < 0 or nx >= n or ny < 0 or ny >= m:
+#                 continue
+#             # 벽인 경우 무시
+#             if graph[nx][ny] == 0:
+#                 continue
+#             # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+#             if graph[nx][ny] == 1:
+#                 graph[nx][ny] = graph[x][y] + 1
+#                 queue.append((nx, ny))
+#     # 가장 오른쪽 아래까지의 최단 거리 반환
+#     return graph[n - 1][m - 1]
 
-print(bfs(0,0))
+# print(bfs(0,0))
+
+
+###################### 성적순 정렬 ######################
+
+Input = list(input().split())
+print(type(Input[1]))
+# Input =[
+#     '홍길동 95',
+#     '이순신 77'
+# ]
+
+for i in range(len(Input)):
+    for j in range(2):
+        Input.append(Input[i][j].split())
+
+print(Input)
