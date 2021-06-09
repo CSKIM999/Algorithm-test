28d79f62504c1d7866d2ddb0b7b68f2bc63effb5
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2021-06-09 22:58:58 +0900

    21/06/09 22:58

--- test_example.py
+++ test_example.py
@@ -39,50 +39,66 @@ Given ) 정수로 이루어진 문자열 S 가 주어짐 // 1 <= S <= 20
 1회차 > 그리디 알고리즘
 '''
 
-# data = list(map(int,input()))
+# # data = input()
+# data = '02984'
 # final = 0
 
-# def max_value(final,index):
-#     if int(data[index+1]) is not int:
-#         return
-#     else:
-#         S = data[index] + data[index+1]
-#         M = data[index] * data[index+1]
-#         value = max(S,M)
-#         final += value
-#         max_value(final,index)
-#         index+=1
-    
-#     return value
-
 # for i in range(len(data)-1):
-#     data[i+1] = max_value(i)
-#     final = data[i+1]
+#     if i == 0:
+#         s = int(data[i]) + int(data[i+1])
+#         m = int(data[i]) * int(data[i+1])
+#         value = max(s,m)
+#         final+=value
+#     else:
+#         s = final + int(data[i+1])
+#         m = final * int(data[i+1])
+#         value = max(s,m)
+#         final = value
 
-# max_value(final,0)
 # print(final)
 
-# print(final)
-
-# data = input()
-# print(int(data[0]))
-# print(data)
 
 
 ############################################  문자열 뒤집기  ############################################
-data = input()
-if data[0] == '0':
-    count_1 = 0
-    count_0 = 1
-elif data[0] == '1':
-    count_1 = 1
-    count_0 = 0
+# data = input()
+# if data[0] == '0':
+#     count_1 = 0
+#     count_0 = 1
+# elif data[0] == '1':
+#     count_1 = 1
+#     count_0 = 0
 
-for i in range(len(data)-1):
-    if data[i] == '0' and data[i+1] == '1':
-        count_1 +=1
-    elif data[i] == '1' and data[i+1] == '0':
-        count_0 +=1
+# for i in range(len(data)-1):
+#     if data[i] == '0' and data[i+1] == '1':
+#         count_1 +=1
+#     elif data[i] == '1' and data[i+1] == '0':
+#         count_0 +=1
     
-print(min(count_0,count_1))
+# print(min(count_0,count_1))
     
+
+############################################  만들 수 없는 금액  ############################################
+'''
+Given ) 동전의 수 : N // 1 <= N <= 1,000
+Input ) 동전의 개수 N 입력 후 동전의 단위 N개 입력
+Output) 해당 동전들로 만들 수 없는 최솟값
+
+그리디
+'''
+n = 5
+data = [3,2,1,1,9,5,4,8]
+data.sort(reverse=True)
+index = max(data)
+target = 0
+
+while True:
+    target +=1
+    v = target
+    for i in range(len(data)):
+        if v >= data[i]:
+            v -= data[i]
+    if v !=0:
+        print(target)
+        break
+
+        
