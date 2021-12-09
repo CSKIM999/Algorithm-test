ca0d7b87728faac33c0299ae94e9585d165fc3d6
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2021-12-09 22:39:57 +0900

    21/12/09 22:39

--- BOJ_2.py
+++ BOJ_2.py
@@ -833,36 +833,102 @@ Output) 한 칸에 말이 4개 이상 올라간다면 그 즉시 게임은 종�
         게임이 종료되는 턴의 번호를 출력한다. 그 값이 1000을 넘어간다면, -1을 출력한다.
 '''
 direction = [[0,1],[0,-1],[-1,0],[1,0]]
-n,k = 4,4
-data = [
-    [0, 0, 2, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 2],
-    [0, 2, 0, 0]
-]
+# n,k = 4,4
+# data = [
+#     [0, 0, 2, 0],
+#     [0, 0, 1, 0],
+#     [0, 0, 1, 2],
+#     [0, 2, 0, 0]
+# ]
+# dot = [[1,0,0],[2,1,2],[1,1,0],[3,0,1]]
 
+n,k = map(int,input().split())
+data = []
+dot = []
+for i in range(n):
+    data.append(list(map(int,input().split())))
+for j in range(k):
+    a,b,c = map(int,input().split())
+    dot.append([a-1,b-1,c-1])
 for i in range(n):
     data[i] = [2]+data[i]+[2]
 data = [[2]*(n+2)] + data + [[2]*(n+2)]
-
 socket = [[[] for _ in range(n)] for _ in range(n)]
-
-dot = [[1,0,0],[2,1,2],[1,1,0],[3,0,1]]
 for i,a in enumerate(dot):
     x,y,d =a
     socket[x][y].append(i)
-xprint(socket)
+flag = True
+result = 1
+
+
+def check():
+    for i in range(n):
+        for j in range(n):
+            if len(socket[i][j]) >= 4:
+                return False
+    return True
+
 def white(idx):
     x,y,d = dot[idx]
     I = socket[x][y].index(idx)
-    nx,ny = x+direction[d[0]],y+direction[d[1]]
-    socket[nx][ny].append(socket[x][y][I:])
+    nx,ny = x+direction[d][0],y+direction[d][1]
+    C = socket[x][y][I:]
+    for n1 in C:
+        nd = dot[n1][2]
+        dot[n1] = [nx,ny,nd]
+    dot[idx] = [nx,ny,d]
+    socket[nx][ny] += (socket[x][y][I:])
     del socket[x][y][I:]
 
-for i in range(
-    
-)
+def red(idx):
+    x,y,d = dot[idx]
+    I = socket[x][y].index(idx)
+    nx,ny = x+direction[d][0],y+direction[d][1]
+    C = socket[x][y][I:]
+    for n1 in C:
+        nd = dot[n1][2]
+        dot[n1] = [nx,ny,nd]
+    socket[nx][ny] += list(reversed(socket[x][y][I:]))
+    del socket[x][y][I:]
+
+def blue(idx):
+    x,y,d = dot[idx]
+    if d%2 == 1:
+        d-=1
+    else:
+        d+=1
+    dot[idx] = [x,y,d]
+    nx,ny = x+direction[d][0],y+direction[d][1]
+    if data[nx+1][ny+1] == 2:
+        return
+    elif data[nx+1][ny+1] == 1:
+        red(idx)
+    else:
+        white(idx)
+
+def operation(lst):
+    global flag
+    for i in range(len(lst)):
+        x,y,d = lst[i]
+        nx,ny = x+direction[d][0],y+direction[d][1]
+        if data[nx+1][ny+1] == 1:
+            red(i)
+        elif data[nx+1][ny+1] == 2:
+            blue(i)
+        else:
+            white(i)
+        if not check():
+            flag = False
+            break
 
 
-def rule(xyd):
-    x,y,d = xyd
+while True:
+    operation(dot)
+    if flag == False:
+        break
+    if result > 1000:
+        result = -1
+        break
+    result += 1
+
+print(result)
