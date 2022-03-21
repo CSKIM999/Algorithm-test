1fa0fa61e15d71822585af5d8e84fba400c59b6e
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2022-03-21 23:28:42 +0900

    22/03/21 23:28 from desktop

--- BOJ_/Q2573.py
+++ BOJ_/Q2573.py
@@ -1,6 +1,22 @@
-import sys
-input = sys.stdin.readline
+from lib import xprint,Prepare_Coding_Test
+Prepare_Coding_Test()
+'''
+BOJ_ QuestionNumber __ Q2573
+#######  TODAY  #######
+##### 2022. 03. 21 #####
+GIVEN ) n*m 크기의 테이블에 빙산의 크기와 위치가 주어진다. 주변에 0 의 갯수에 따라 녹는 속도가 달라진다고 했을 때,
+        빙산의 덩어리가 2개 이상으로 갈라지는 시간을 출력하라
+INPUT ) 첫째 줄에 N,M 이 주어진다 ( 3<= N,M <= 300 )
+        그 다음 N 개의 줄에 걸쳐 테이블의 상태가 주어진다. 0 은 빈칸, 나머지는 10 이하의 정수로 주어진다.
+OUTPUT) 빙산이 분리되는 최초의 시간을 출력하라. 만약 다 녹았음에도 분리되지 않으면 0을 출력하라
+
+Approach )  제일 큰 빙산의 좌표를 저장하고 녹는bfs 돌린 이후 테이블 bfs 돌려서 카운트 2개이상 혹은 0 개 나오면 break 하게 하면 안되나?
+'''
+# import sys
+# input = sys.stdin.readline
+
 from collections import deque
+
 n,m = map(int,input().split())
 table = []
 d = [[-1,0],[0,1],[1,0],[0,-1]]
@@ -77,7 +93,3 @@ else:
         else:
             bigM = melt_bfs(bigM)
         time += 1
-
-'''
-1회차 시간초과??
-'''
