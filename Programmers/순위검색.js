f1f6b8870132d8932b3819b7b0c37f7f57723437
CSKIM999 <75712211+CSKIM999@users.noreply.github.com>
2022-11-10 23:43:15 +0900

    22/11/10 stressful
    23:43 from desktop

--- Programmers/순위검색.js
+++ Programmers/순위검색.js
@@ -1,15 +1,17 @@
+const cl = console.log;
+
 function solution(info, query) {
   const binarySearch = (array, targetValue) => {
     let left = 0;
     let right = array.length - 1;
-    if (array[right] < targetValue) {
-      return -1;
-    } else if (right === 0) {
-      return 0;
-    }
+
     while (left <= right) {
       if (right - left <= 1) {
-        return left + 1;
+        if (array[left] === targetValue) {
+          return left;
+        } else {
+          return left + 1;
+        }
       }
       let mid = Math.floor((left + right) / 2);
       if (array[mid] >= targetValue) {
@@ -20,8 +22,35 @@ function solution(info, query) {
     }
   };
 
+  const BS = (array, target) => {
+    var left = 0;
+    var right = array.length - 1;
+    if (target <= array[0]) {
+      return 0;
+    } else if (target > array[right]) {
+      return right;
+    }
+    while (left <= right) {
+      if (right - left == 1) {
+        if (array[left] === target) {
+          return left;
+        }
+        return left + 1;
+      }
+      var mid = Math.floor((right + left) / 2);
+      if (array[mid] >= target) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+  };
+
   var answer = [];
   var dic = {};
+  const newInfo = Array(info)[0];
+  const Q = Array(query)[0];
+
   for (lang of ["java", "python", "cpp", "-"]) {
     for (group of ["frontend", "backend", "-"]) {
       for (career of ["junior", "senior", "-"]) {
@@ -30,9 +59,6 @@ function solution(info, query) {
       }
     }
   }
-
-  const newInfo = Array(info)[0];
-  const Q = Array(query)[0];
 
   for (item of newInfo) {
     const I = item.split(" ").filter((item) => item !== "and");
@@ -46,6 +72,7 @@ function solution(info, query) {
       }
     }
   }
+
   for (lang of ["java", "python", "cpp", "-"]) {
     for (group of ["frontend", "backend", "-"]) {
       for (career of ["junior", "senior", "-"]) {
@@ -54,12 +81,39 @@ function solution(info, query) {
       }
     }
   }
+
   for (item of Q) {
     const I = item.split(" ").filter((item) => item !== "and");
     answer.push(
       dic[I[0] + I[1] + I[2] + I[3]].length -
-        binarySearch(dic[I[0] + I[1] + I[2] + I[3]], I[4])
+        BS(dic[I[0] + I[1] + I[2] + I[3]], +I[4])
     );
   }
+
+  const test = [1, 2, 3, 4, 5, 6, 7, 8, 9].sort((a, b) => a - b);
+  const testNum = 10;
+  console.log(">>>> test", BS(test, testNum));
   return answer;
 }
+
+console.log(
+  solution(
+    [
+      "java backend junior pizza 150",
+      "python frontend senior chicken 210",
+      "python frontend senior chicken 150",
+      "cpp backend senior pizza 260",
+      "java backend junior chicken 80",
+      "python backend senior chicken 50",
+    ],
+    [
+      "java and backend and junior and pizza 100",
+      "python and frontend and senior and chicken 200",
+      "cpp and - and senior and pizza 250",
+      "- and backend and senior and - 150",
+      "- and - and - and chicken 100",
+      "- and - and - and - 150",
+      "- and - and - and - 5000",
+    ]
+  )
+);
--- test.py
+++ test.py
@@ -1,32 +1,28 @@
-n = 50000
+order = int(input())
 
-dp = [0]*(n+1)
-dp[1] = 1
-for i in range(2, n+1):
-    for j in range(1, int(i**0.5)+1):
-        dp[i] = min(4, dp[i-j**2]+1)
-print(dp[n])
+rest = order % 5
+cnt = order // 5
 
-dp = [0, 1]
-for i in range(2, n + 1):
-    target = 4
-    for j in range(1, 50001):
-        if j ** 2 > i:
-            break
-        target = min(target, dp[i - (j**2)])
-    dp.append(target + 1)
-print(dp[n])
+if rest == 0:
+    print(cnt)
 
+elif rest == 1:
+    if cnt == 0:
+        print(-1)
+    else:
+        print(cnt + 1)
 
-dp = [0, 1]
+elif rest == 2:
+    if cnt < 2:
+        print(-1)
+    else:
+        print(cnt + 2)
 
-for i in range(2, n+1):
-    min_value = 1e9
-    j = 1
+elif rest == 3:
+    print(cnt + 1)
 
-    while (j**2) <= i:
-        min_value = min(min_value, dp[i - (j**2)])
-        j += 1
-
-    dp.append(min_value + 1)
-print(dp[n])
+else:
+    if cnt == 0:
+        print(-1)
+    else:
+        print(cnt + 2)
