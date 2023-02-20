6c8f12e48930c3a709d3b71264016d5c4cab8294
CSKIM <rlackstjq987@gmail.com>
2023-02-20 17:29:16 +0900

    23/02/20 pp . .
    17:29 from laptop

--- BOJ_/Solved/Q22866.py
+++ BOJ_/Solved/Q22866.py
@@ -1,8 +1,30 @@
+import sys
+from lib import xprint, Prepare_Coding_Test
+Prepare_Coding_Test()
+'''
+BOJ_ QuestionNumber __ Q22866
+#######  TODAY  #######
+##### 2023. 02. 20 #####
+GIVEN ) 일직선 상에 건물들이 위치한다.
+        바라보는 방향에서 건물들은 일직선 상에 위치하기에 같거나 작은 높이라면 보이지 않는다.
+        해당 조건에서 각 건물의 옥상에서 양옆으로 보이는 건물의 수를 찾고자 한다.
+        
+
+INPUT ) 
+
+OUTPUT) 
+
+Approach )  
+'''
+sys.setrecursionlimit(25000)
+# input = sys.stdin.readline
 n = int(input())
 table = list(map(int, input().split()))
 left, right = [[table[-1], n-1]], [[table[0], 0]]
 dt = {i: [0, 0] for i in range(n)}
 pivot = {i: [None, None] for i in range(n)}
+# 8
+# 3 7 1 6 3 5 1 7
 
 
 def stackAppend(arr, num, now, numi):
@@ -48,6 +70,15 @@ for i in range(n):
         if al <= ar:
             print(value, l+1)
         else:
-            print(value, r+1)
+            print(value, r+1)  # 항상 index에서 문제가 생긴다.
     else:
         print(value)
+
+
+'''
+이번에도 Index 부분에서 오류가 생김
+인덱스 처리에서 헷갈리지 않을 확실한 방법이 좀 필요해보임.
+시간낭비도 시간낭비인데 틀리는 경우도 너무 많음
+
+
+'''
--- test.py
+++ test.py
@@ -1,43 +1,18 @@
-answer = 0
-a = "5 1 4 3 4 2 5 6"
-arr = list(map(int, a.split()))
-l = len(arr)
-s = []
-pivot = 0
-for i in range(l):
-    now = arr[i]
-    if now > pivot:
-        pivot = now
-        if s:
-            start = s[-1][0]
-            s.append([i, start, now])
-        else:
-            s.append([i, -1, now])
-    else:  # now <= pivot now 가 피벗보다 작으면? 무조건 s 가 있는거 아님?
-        pivot = now
-        while True:
-            if not s:
-                s.append([i, -1, now])
-                break
-            a, st, v = s[-1]
-            if v >= now:
-                value = (i-st-1)*v
-                answer = max(answer, value)
-                s.pop()
-            else:
-                s.append([i, a, now])
-                break
-
-
-while s:
-    act, st, val = s[-1]
-    value = (l-st-1)*val
-    answer = max(answer, value)
-    s.pop()
-
-print(answer)
-
 '''
-0 index 처리가 헷갈려서 애먹은 문제.
-이런건 진짜 시간들이면 안되는건디;;
+5
+1 10
+1 10
+1 10
+1 10
+1 10
 '''
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [0 for _ in range(n+1)]
+for i in range(n):
+    req, value = arr[i]
+    if req+i > n:
+        continue
+    dp[req+i] = dp[i]+value
+
+print(max(dp))
